96ade28a722132268f7126bbc05cb3c0c3a2f370
Madoww <filiplszafran@gmail.com>
2023-08-22 20:37:27 +0200

    Added EditorToolbox.

---
+++ Assets/Editor Toolbox/Runtime/Serialization/SceneSerializationUtility.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine.SceneManagement;
+
+namespace Toolbox.Serialization
+{
+    internal static class SceneSerializationUtility
+    {
+#if UNITY_EDITOR
+        private static readonly Dictionary<SceneAsset, SceneData> cachedScenes = new Dictionary<SceneAsset, SceneData>();
+        private static bool isInitialized;
+
+
+        [InitializeOnLoadMethod]
+        private static void Initialize()
+        {
+            if (isInitialized)
+            {
+                return;
+            }
+
+            ConfirmCache();
+            EditorBuildSettings.sceneListChanged -= RefreshCache;
+            EditorBuildSettings.sceneListChanged += RefreshCache;
+            isInitialized = true;
+        }
+
+        internal static void ConfirmCache()
+        {
+            //NOTE: refresh data only if the cache is empty,
+            //it probably means that it's our first time when we are updating it
+            if (cachedScenes.Count == 0)
+            {
+                RefreshCache();
+            }
+        }
+
+        internal static void RefreshCache()
+        {
+            cachedScenes.Clear();
+            foreach (var scene in EditorBuildSettings.scenes)
+            {
+                var path = scene.path;
+                if (string.IsNullOrEmpty(path))
+                {
+                    continue;
+                }
+
+                var sceneAsset = EditorGUIUtility.Load(path) as SceneAsset;
+                if (sceneAsset == null)
+                {
+                    continue;
+                }
+
+                if (cachedScenes.ContainsKey(sceneAsset))
+                {
+                    continue;
+                }
+
+                cachedScenes.Add(sceneAsset, new SceneData()
+                {
+                    BuildIndex = SceneUtility.GetBuildIndexByScenePath(path),
+                    SceneName = sceneAsset.name,
+                    ScenePath = path
+                });
+            }
+        }
+
+
+        public static bool TryGetSceneData(SceneAsset sceneAsset, out SceneData data)
+        {
+            ConfirmCache();
+            if (!sceneAsset || !cachedScenes.TryGetValue(sceneAsset, out data))
+            {
+                data = null;
+                return false;
+            }
+
+            return true;
+        }
+#endif
+
+
+        public static int InvalidSceneIndex => -1;
+    }
+}
