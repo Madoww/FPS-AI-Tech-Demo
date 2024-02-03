ec15c1ff2e9c0e77b396106e1cdc8af1bfc36c52
Madoww <filiplszafran@gmail.com>
2024-02-03 08:38:22 +0100

    Adding UI.

---
+++ Assets/Scripts/UI/IActivityTarget.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FPS.UI
+{
+    public class IActivityTarget : MonoBehaviour
+    {
+
+    }
+}
---
+++ Assets/Scripts/UI/UiHandler.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FPS.UI
+{
+    public class UiHandler : IUiHandler
+    {
+        public event Action OnInitialized;
+        public event Action OnDeinitialized;
+
+        public bool IsInitialized { get; private set; }
+
+        public void Initialize()
+        {
+            if(IsInitialized)
+            {
+                return;
+            }
+
+            OnInitialize();
+            IsInitialized = true;
+            OnInitialized?.Invoke();
+        }
+
+        public void Deinitialize()
+        {
+            if(!IsInitialized)
+            {
+                return;
+            }
+
+            OnDeinitialize();
+            IsInitialized = false;
+            OnDeinitialized?.Invoke();
+        }
+
+        protected virtual void OnInitialize()
+        { }
+
+        protected virtual void OnDeinitialize()
+        { }
+    }
+}
